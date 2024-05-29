f0cff987f94b4ca084c1458b88baf0221d0f8c61
OTAKUWeBer <www.tahsin3@gmail.com>
2024-05-30 00:29:38 +0600

    Deleted directories and files: .config/rafi, .config/nvim, .config/assests/bins/swww, .config/assests/bins/swww-deamon, .config/assests/bins/swww_source.txt

--- .config/nvim/lua/plugins/presence_nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"andweeb/presence.nvim",
-	config = function()
-		require("presence").setup({
-			neovim_image_text = "The One True Text Editor",
-			main_image = "file",
-		})
-	end,
-}
--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.keymap.set("n", "<leader>t", function()
-			require("trouble").toggle()
-		end)
-	end,
-}
