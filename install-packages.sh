bacbd94cd7c68c52aa0cf25a244caf9bbfc3bbd3
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2024-07-31 00:52:16 +0600

    Modified

--- install-packages.sh
+++ install-packages.sh
@@ -15,4 +15,4 @@ fi
 sudo pacman -Syu --noconfirm base-devel networkmanager network-manager-applet pipewire pavucontrol waybar wf-recorder bemenu-wayland foot wireguard-tools gnome-keyring git resolvconf xorg-xwayland mako cliphist github-cli stow sway zsh nodejs npm slurp grim thunar polkit xdg-desktop-portal xdg-desktop-portal-wlr vlc ttf-cascadia-code-nerd
 
 # Install AUR packages with paru
-paru -S --noconfirm wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag pulseaudio noto-fonts-emoji blueman
+paru -S --noconfirm wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag pulseaudio noto-fonts-emoji blueman visual-studio-code-bin swaylock
