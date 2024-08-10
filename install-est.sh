274b3a4d15535f3aef469c570948dd2179945031
WEBER <137557891+OTAKUWeBer@users.noreply.github.com>
2024-08-10 08:25:03 +0600

    Update install-est.sh

--- install-est.sh
+++ install-est.sh
@@ -2,4 +2,5 @@
 
 
 # Update and install packages with pacman
-sudo pacman -Syu --noconfirm base-devel networkmanager network-manager-applet pipewire pavucontrol waybar wf-recorder bemenu-wayland foot wireguard-tools gnome-keyring resolvconf xorg-xwayland mako cliphist github-cli stow sway zsh nodejs npm slurp grim thunar polkit xdg-desktop-portal xdg-desktop-portal-wlr vlc ttf-cascadia-code-nerd
+sudo pacman -Syu --noconfirm base-devel networkmanager network-manager-applet pipewire pavucontrol waybar wf-recorder bemenu-wayland foot wireguard-tools gnome-keyring resolvconf xorg-xwayland mako cliphist github-cli stow sway zsh nodejs npm slurp grim thunar polkit xdg-desktop-portal xdg-desktop-portal-wlr 
+
