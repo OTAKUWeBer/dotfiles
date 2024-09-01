6ad527b49c29f3064675c5ea2455910d6d4f45fd
weberz <137557891+OTAKUWeBer@users.noreply.github.com>
2024-09-01 11:07:04 +0600

    Modified

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install AUR packages with paru
-paru -S --noconfirm wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji blueman visual-studio-code-bin vlc ttf-cascadia-code-nerd
+paru -S wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji blueman visual-studio-code-bin vlc ttf-cascadia-code-nerd
