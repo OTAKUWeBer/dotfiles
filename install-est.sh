4c2ba99494ce4c049b5691ecd7bbdf17812372c8
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2024-09-14 18:58:19 +0600

    Modified

--- install-est.sh
+++ install-est.sh
@@ -3,4 +3,3 @@
 
 # Update and install packages with pacman
 sudo pacman -Syu gammastep wget unzip ripgrep wf-recorder ffmpegthumbnailer poppler-glib libgsf libgepub libopenraw freetype2 tumbler rsync mesa-amber qt5-wayland qt6-wayland zoxide noto-fonts-cjk p7zip noto-fonts-emoji base-devel networkmanager network-manager-applet pipewire pavucontrol waybar bemenu-wayland foot wireguard-tools gnome-keyring git resolvconf xorg-xwayland mako cliphist github-cli stow sway zsh nodejs npm slurp grim thunar polkit xdg-desktop-portal xdg-desktop-portal-wlr vlc ttf-cascadia-code-nerd
-
