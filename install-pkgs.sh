994b05c71e15f0a43e0ac325ba9152dbef00a943
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2024-09-26 22:40:22 +0600

    Modified

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install AUR packages with paru
-paru -S swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji neofetch wlogout
+paru -S swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji neofetch wlogout rofi
