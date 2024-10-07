f72134d1703311ffa5ff068f45c47a13ff08fabd
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2024-10-07 16:35:20 +0600

    Modified

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-paru -S swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji neofetch wlogout rofi playerctl fastfetch swaylock-effects gvfs gnome-calculator zoxide
+paru -S swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock oh-my-zsh swaybg brightnessctl swaynag noto-fonts-emoji neofetch wlogout rofi playerctl fastfetch swaylock-effects gvfs gnome-calculator zoxide motrix-bin
