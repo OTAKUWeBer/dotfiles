01f917ef25139aef5bb15496e46c25118123230a
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2025-02-11 15:09:51 +0600

    fcitx5 bangla

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-paru -S --skipreview swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock swaybg brightnessctl neofetch wlogout rofi playerctl fastfetch swaylock-effects gvfs gnome-calculator zoxide motrix-bin rofi-emoji localsend-bin hardinfo cava papirus-icon-theme
+paru -S --skipreview swww wl-clip-persist-git catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git swaylock swaybg brightnessctl neofetch wlogout rofi playerctl fastfetch swaylock-effects gvfs gnome-calculator zoxide motrix-bin rofi-emoji localsend-bin hardinfo cava papirus-icon-theme fcitx5 fcitx5-configtool fcitx5-m17n
